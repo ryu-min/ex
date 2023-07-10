b14fd615e3be4a567379bc57e32627de728b8173
Ryumin Sergey <90983639+ryu-min@users.noreply.github.com>
2023-07-10 23:38:14 +0300

    some progress in parsing

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ use std::collections::HashSet;
     Var,
     Dot,
     Assignment,
+    Plus,
+    Minus,
+    Multi,
+    Devide,
     OpenBrace,
     CloseBrace,
     OpenCurlyBrace,
@@ -27,86 +31,34 @@ use std::collections::HashSet;
             Token::Exec => write!(f, "EXEC TOKEN"),
             Token::OpenBrace => write!(f, "OPEN BRACE TOKEN"),
             Token::CloseBrace => write!(f, "CLOSE BRACE TOKEN"),
-            Token::OpenCurlyBrace => write!(f, "OPEN CURLY BRACE"),
-            Token::CloseCurlyBrace => write!(f, "CLOSE CURLY BRACE"), 
+            Token::OpenCurlyBrace => write!(f, "OPEN CURLY BRACE TOKEN"),
+            Token::CloseCurlyBrace => write!(f, "CLOSE CURLY BRACE TOKEN"),
+            Token::Plus => write!(f, "PLUS TOKEN"),
+            Token::Minus => write!(f, "MINUS TOKEN"),
+            Token::Multi => write!(f, "MULTI TOKEN"),
+            Token::Devide => write!(f, "DEVIDE TOKEN"), 
         }
     }
 }
 
-fn tokenize(program: &str) -> Vec<Token> {
-    let mut result: Vec<Token> = Vec::new();
-    for line in program.lines().clone() {
-        let splited_word = line.split(" ");
-        for word in splited_word {
-            if !word.is_empty() {
-                if let Some(tokens) = str_to_token(word) {
-                    for token in tokens {
-                        result.push(token);
-                    }
-                } else {
-                    println!("can't parse string {} as token", word)
-                }
-            }
-        }
-    }
-    return result;
-}
-
-fn str_to_token(str_value: &str) -> Option<Vec<Token>>{
-    let mut source = str_value.to_string();
-    if str_value.is_empty() {
-        return Some(Vec::new());
-    }
-    match str_value {
-        "var" =>    Some(vec![Token::Var]),
-        "." =>      Some(vec![Token::Dot]),
-        "=" =>      Some(vec![Token::Assignment]),
-        "exec" =>   Some(vec![Token::Exec]),
-        "(" =>      Some(vec![Token::OpenBrace]),
-        ")" =>      Some(vec![Token::CloseBrace]),
-        "{" =>      Some(vec![Token::OpenCurlyBrace]),
-        "}" =>      Some(vec![Token::CloseCurlyBrace]),
-        _ => {
-            if let Ok(i) = str_value.parse::<i64>() {
-                return Some(vec![Token::IntLiteral(i)]);
-            }
-            if let Ok(f) = str_value.parse::<f64>() {
-                return Some(vec![Token::FloatLiteral(f)]);
-            } 
-            if let Some(first_char) = str_value.chars().next() {
-                if first_char == ')' || first_char == '(' {
-                    if let Some(mut first_tokens) = str_to_token(first_char.to_string().as_str()) {
-                        if let Some(second_tokens) = str_to_token(&str_value[1..]) {
-                            for token in second_tokens {
-                                first_tokens.push(token)
-                            }
-                        }
-                        return Some(first_tokens);
-                    }
-                    else {
-                        assert!(true, "unreachable code");
-                        return None
-                    }
-                }
-                else {
-                    if let Some(str_literal) = read_string_literal(&mut source) {
-                        let mut result = Vec::new();
-                        result.push(str_literal);
-                        if let Some(other_tokens) = str_to_token(&mut source) {
-                            for token in other_tokens {
-                                result.push(token);
-                            }
-                        }
-                        return Some(result);
-                     } else {
-                        return Some(Vec::new());
-                     }     
-                }
-            }
-            None
-        }
-    }
-}
+// fn tokenize(program: &str) -> Vec<Token> {
+//     let mut result: Vec<Token> = Vec::new();
+//     for line in program.lines().clone() {
+//         let splited_word = line.split(" ");
+//         for word in splited_word {
+//             if !word.is_empty() {
+//                 if let Some(tokens) = str_to_token(word) {
+//                     for token in tokens {
+//                         result.push(token);
+//                     }
+//                 } else {
+//                     println!("can't parse string {} as token", word)
+//                 }
+//             }
+//         }
+//     }
+//     return result;
+// }
 
 
 fn char_to_token(ch : char) -> Option<Token> {
@@ -117,27 +69,14 @@ fn char_to_token(ch : char) -> Option<Token> {
         ')' =>      Some(Token::CloseBrace),
         '{' =>      Some(Token::OpenCurlyBrace),
         '}' =>      Some(Token::CloseCurlyBrace),
+        '+' =>      Some(Token::Plus),
+        '-' =>      Some(Token::Minus),
+        '/' =>      Some(Token::Devide),
+        '*' =>      Some(Token::Minus),
         _ =>        None
     }
 }
 
-fn string_to_token(source: &str) -> Option<Token>{
-    match source {
-        "var" =>    Some(Token::Var),
-        "exec" =>   Some(Token::Exec),
-        _      =>   {
-            if let Ok(i) = source.parse::<i64>() {
-                return Some(Token::IntLiteral(i));
-            }
-            else if let Ok(f) = source.parse::<f64>() {
-                return Some(Token::FloatLiteral(f));
-            }
-            else {
-                return Some(Token::StringLiteral(source.to_string()))
-            }
-        }
-    }
-}
 
 fn read_token_from_char(source: &mut String) -> Option<Token> {
     if let Some(ch) = source.chars().next() {
@@ -150,35 +89,67 @@ fn read_token_from_char(source: &mut String) -> Option<Token> {
 }
 
 fn read_token_from_string(source: &mut String) -> Option<Token> {
-    let mut word = String::new();
-    for ch in source.chars() {
-        if ch.is_alphanumeric() && !get_reserved_chars().contains(&ch) {
-            word.push(ch)
-        }
-    }
-    println!("word is {word}");
-    if word.is_empty() {
+
+    if source.is_empty() {
         return None
     }
-    if let Some(token) = string_to_token(&word) {
-        source.replace_range(0..word.len(), "");
+    if let Some(token) = read_number_token(source) {
+        return Some(token)
+    } else if let Some(token) = read_reserved_token(source) {
         return Some(token);
+    }  else if let Some(token) = read_string_literal_token(source) {
+        return Some(token);
+    } 
+    println!("return none from read token");
+    None
+}
+
+
+
+fn read_number_token(source: &mut String) -> Option<Token> {
+    let mut result_string = String::new();
+    for (i, ch) in source.char_indices() {
+        if i == 0 {
+            if ch != '-' && ch != '+' && !ch.is_numeric() {
+                return None
+            }
+            result_string.push(ch);
+        } else {
+            if ch.is_numeric() || ch == '.' {
+                result_string.push(ch);
+            } else {
+                break;
+            }    
+        }
+    }
+    if result_string.is_empty() {
+        return None
+    }
+    source.replace_range(0..result_string.len(), "");
+    if let Ok(f) = result_string.parse::<f64>() {
+        println!("return {f}");
+        return Some(Token::FloatLiteral(f));
+    } else if let Ok(i) = result_string.parse::<i64>() {
+        println!("return {i}");
+        return Some(Token::IntLiteral(i));
     }
     None
 }
 
-/// @todo
-/// in tokenize method
-/// untill programm is not empty
-/// 1. try to read char
-/// 2. try to read string
-/// collect all tokens in progress
 
+fn read_reserved_token(source: &mut String) -> Option<Token> { 
+    // @todo write source.clear somewhere once
+    if source == "exec" {
+        source.clear();
+        return Some(Token::Exec);
+    } else if source == "var" {
+        source.clear();
+        return Some(Token::Var);
+    } 
+    None
+}
 
-
-
-
-fn read_string_literal(source: &mut String) -> Option<Token> {
+fn read_string_literal_token(source: &mut String) -> Option<Token> {
     let mut result = String::new();
     for ch in source.chars() {
         if ch.is_alphanumeric() {
@@ -187,19 +158,64 @@ fn read_string_literal(source: &mut String) -> Option<Token> {
             break;
         }
     }
-    if !result.is_empty() {
-        source.replace_range(0..result.len(), "");
-        if let Some( tokens ) = str_to_token(&result) {
-            if tokens.len() == 1 {
-                return Some(tokens[0].clone());
-            } 
-        }
+    if result.is_empty() {
+        return None;
     }
-    None
+    source.replace_range(0..result.len(), "");
+    return Some(Token::StringLiteral(result));
 }
 
 
+//     for line in program.lines().clone() {
+//         let splited_word = line.split(" ");
+//         for word in splited_word {
+//             if !word.is_empty() {
+//                 if let Some(tokens) = str_to_token(word) {
+//                     for token in tokens {
+//                         result.push(token);
+//                     }
+//                 } else {
+//                     println!("can't parse string {} as token", word)
+//                 }
+//             }
+//         }
+//     }
+//     return result;
 
+
+
+fn tokenize(program: &String) -> Vec<Token> {
+    let mut program_local = program.clone();
+    let mut result : Vec<Token> = Vec::new();
+    for line in program.lines().clone() {
+        println!("new line {line}");
+        let splited_word = line.split(" ");
+        for word in splited_word { 
+            let mut word_string = word.to_string();
+            while !word_string.is_empty() {
+                if let Some(token) = read_token_from_char(&mut word_string) {
+                    result.push(token)
+                } else if let Some(token) = read_token_from_string(&mut word_string) {
+                    result.push(token)    
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    // while !program_local.is_empty() {
+    //     println!("program local {program_local}");
+    //     if let Some(token) = read_token_from_char(&mut program_local) {
+    //         result.push(token)
+    //     } else if let Some(token) = read_token_from_string(&mut program_local) {
+    //         result.push(token)    
+    //     } else {
+    //         break;
+    //     }
+    // }
+    return result;
+}
 
 fn get_reserved_chars() -> HashSet<char> {
     let mut reserved : HashSet<char> = HashSet::new();
@@ -209,17 +225,18 @@ fn get_reserved_chars() -> HashSet<char> {
     reserved.insert('{');
     reserved.insert('}');
     reserved.insert('=');
+    reserved.insert('+');
+    reserved.insert('-');
+    reserved.insert('*');
+    reserved.insert('/');
     return reserved;
 }
 
 
 fn main() -> io::Result<()>{
-
-
-
-
-    let program = "4.44)";
-    let tokens = tokenize(program);
+    let mut program = "(4.44+a+33-22.3 sdfsdf)){})\n 1 + 2";
+    //program = "3 + 2 + {}";
+    let tokens = tokenize(&program.to_string());
     for token in tokens {
         println!("token is {}", token.to_string());
     }
