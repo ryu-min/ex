3d98fd3bc6180d72b1d45853118dac22b1648072
Ryumin Sergey <90983639+ryu-min@users.noreply.github.com>
2023-09-09 12:19:53 +0300

    remove paranthersis in method.ex

--- examples/method.ex
+++ examples/method.ex
@@ -2,9 +2,9 @@ a = 2
 
 write("2 ^ 2 == ",a.pow(2)) 
 writeln()
-write ("2 ^ 4 == ", (2).pow(4)) 
+write ("2 ^ 4 == ", 2.pow(4)) 
 writeln()
-write("2 ^ 8 == ", (2).pow(4).pow(2))
+write("2 ^ 8 == ", 2.pow(4).pow(2))
 writeln()
 
  
